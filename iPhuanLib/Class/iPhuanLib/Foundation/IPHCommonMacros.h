5fe409382ecc7b2bd21a27441054358ef67c332a
贺欢 <iphuan@188w.com>
2017-05-19 17:04:31 +0800

    新增IPH6FitSize宏定义

--- iPhuanLib/Class/iPhuanLib/Foundation/IPHCommonMacros.h
+++ iPhuanLib/Class/iPhuanLib/Foundation/IPHCommonMacros.h
@@ -6,17 +6,6 @@
 //  Copyright © 2017年 iPhuan. All rights reserved.
 //
 
-#pragma mark - System Singleton // 建议在之后的开发过程中直接使用系统方法去获取单列而废弃宏的用法，以减少工程的耦合性
-/*****************************************************************************************/
-#define IPH_APPLICATION         [UIApplication sharedApplication]       // Deprecated
-#define IPH_APP_WINDOW          IPH_APPLICATION.keyWindow               // Deprecated
-#define IPH_NOTIOFICATION       [NSNotificationCenter defaultCenter]    // Deprecated
-#define IPH_USER_DEFAULT        [NSUserDefaults standardUserDefaults]   // Deprecated
-#define IPH_FILE_MANAGER        [NSFileManager defaultManager]          // Deprecated
-#define IPH_BUNDLE              [NSBundle mainBundle]                   // Deprecated
-#define IPH_DEVICE              [UIDevice currentDevice]                // Deprecated
-
-
 
 #pragma mark - Geometry
 /*****************************************************************************************/
@@ -25,6 +14,8 @@
 #define IPHScreenBounds        [UIScreen mainScreen].bounds
 #define IPHSizeMultiple        (IPHScreenWidth/320.0)
 #define IPHFitSize(X)          ((X) * IPHSizeMultiple)
+#define IPH6SizeMultiple       (IPHScreenWidth/375.0)
+#define IPH6FitSize(X)         ((X) * IPH6SizeMultiple)
 
 
 #pragma mark - Conditional Judgment
@@ -43,7 +34,6 @@ string = value; \
 #define IPHRGBColor(r,g,b)         [UIColor colorWithRed:(r)/255.0f green:(g)/255.0f blue:(b)/255.0f alpha:1.0f]
 #define IPHRGBAColor(r,g,b,a)      [UIColor colorWithRed:(r)/255.0f green:(g)/255.0f blue:(b)/255.0f alpha:(a)]
 #define IPHHexColor(c)             [UIColor colorWithRed:((c>>16)&0xFF)/255.0f green:((c>>8)&0xFF)/255.0f blue:(c&0xFF)/255.0f alpha:1.0f]
-
 
 #pragma mark - Others
 /*****************************************************************************************/
@@ -64,3 +54,16 @@ string = value; \
     });                                                     \
     return z##_shared_obj_name_;                            \
 }
+
+
+#pragma mark - Deprecated
+
+#pragma mark - System Singleton // 建议在之后的开发过程中直接使用系统方法去获取单列而废弃宏的用法，以减少工程的耦合性
+/*****************************************************************************************/
+#define IPH_APPLICATION         [UIApplication sharedApplication]       // Deprecated
+#define IPH_APP_WINDOW          IPH_APPLICATION.keyWindow               // Deprecated
+#define IPH_NOTIOFICATION       [NSNotificationCenter defaultCenter]    // Deprecated
+#define IPH_USER_DEFAULT        [NSUserDefaults standardUserDefaults]   // Deprecated
+#define IPH_FILE_MANAGER        [NSFileManager defaultManager]          // Deprecated
+#define IPH_BUNDLE              [NSBundle mainBundle]                   // Deprecated
+#define IPH_DEVICE              [UIDevice currentDevice]                // Deprecated
